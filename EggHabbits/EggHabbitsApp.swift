0f080fca1ef660f4b1d05c5875d3cd12ba3ec7e6
Scott DiBenedetto <82473510+scooterd22@users.noreply.github.com>
2025-02-12 16:30:24 -0800

    Initial Commit

---
+++ EggHabbits/EggHabbitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggHabbitsApp.swift
+//  EggHabbits
+//
+//  Created by Scott DiBenedetto on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct EggHabbitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EggHabbitsTests/EggHabbitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  EggHabbitsTests.swift
+//  EggHabbitsTests
+//
+//  Created by Scott DiBenedetto on 2/12/25.
+//
+
+import Testing
+@testable import EggHabbits
+
+struct EggHabbitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
